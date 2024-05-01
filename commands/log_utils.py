cd4cc1efe6f0656b4be9835b1a1ae1ae01248ff3
Restart <79792966+RestartB@users.noreply.github.com>
2024-05-01 22:12:38 +0100

    Editing logging now works

--- commands/log_utils.py
+++ commands/log_utils.py
@@ -5,6 +5,7 @@ from discord.ui import View
 from discord.ext import commands
 import sqlite3
 #import log_utils
+import asyncio
 
 import discord.ext.tasks
 
@@ -13,6 +14,11 @@ class log_utils(commands.Cog):
         self.bot = bot
         self.connection = sqlite3.connect(f"{self.bot.path}{self.bot.pathtype}content{self.bot.pathtype}sql{self.bot.pathtype}logging.db")
         self.cursor = self.connection.cursor()
+        self.bot.loggingToggle = self.cursor.execute(f"SELECT * FROM 'loggingToggle'").fetchall()
+        self.bot.loggingChannel = self.cursor.execute(f"SELECT * FROM 'loggingChannel'").fetchall()
+        print(self.bot.loggingToggle)
+        print(self.bot.loggingChannel)
+        print("[LOG] Refreshed logging vars.")
 
     async def refreshLoggingVars(self):
         self.bot.loggingToggle = self.cursor.execute(f"SELECT * FROM 'loggingToggle'").fetchall()
@@ -34,12 +40,12 @@ class log_utils(commands.Cog):
     async def enable_logging(self, interaction: discord.Interaction, log_type: app_commands.Choice[str], logging_channel: discord.TextChannel):
         await interaction.response.defer(ephemeral = True)
 
-        embed = discord.Embed(title = "Enabling...", color = Color.orange())
-        await interaction.followup.send(embed = embed, ephemeral = True)
-
         try:
+            embed = discord.Embed(title = "Enabling...", color = Color.orange())
+            await interaction.followup.send(embed = embed, ephemeral = True)
+
             if self.cursor.execute(f"SELECT server_id FROM 'loggingToggle' WHERE server_id = {interaction.guild.id}").fetchone() != None:
-                if self.cursor.execute(f"SELECT {log_type.value} FROM 'loggingToggle' WHERE server_id = {interaction.guild.id}").fetchone() == "False":
+                if (self.cursor.execute(f"SELECT {log_type.value} FROM 'loggingToggle' WHERE server_id = {interaction.guild.id}").fetchone())[0] == "False":
                     self.cursor.execute(f"UPDATE 'loggingChannel' SET {log_type.value} = {logging_channel.id} WHERE server_id = {interaction.guild.id}")
                     self.cursor.execute(f"UPDATE 'loggingToggle' SET {log_type.value} = 'True' WHERE server_id = {interaction.guild.id}")
 
@@ -50,6 +56,7 @@ class log_utils(commands.Cog):
                     embed = discord.Embed(title = "Enabled.", color = Color.green())
                     await interaction.edit_original_response(embed = embed)
                 else:
+                    print((self.cursor.execute(f"SELECT {log_type.value} FROM 'loggingToggle' WHERE server_id = {interaction.guild.id}").fetchone())[0])
                     embed = discord.Embed(title = "Already enabled.", color = Color.green())
                     await interaction.edit_original_response(embed = embed)
             else:
@@ -65,10 +72,9 @@ class log_utils(commands.Cog):
                 
                 embed = discord.Embed(title = "Enabled.", color = Color.green())
                 await interaction.edit_original_response(embed = embed)
-                return
         except Exception:
             embed = discord.Embed(title = "Unexpected Error", description = "Please try again later or message <@563372552643149825> for assistance.", color = Color.red())
-            await interaction.edit_original_response(embed = embed, view = None)
+            await interaction.edit_original_response(embed = embed)
     
     # Disable Logging command
     # Broken
@@ -94,7 +100,7 @@ class log_utils(commands.Cog):
                     self.connection.commit()
 
                     await log_utils.refreshLoggingVars()
-
+                    
                     embed = discord.Embed(title = "Disabled.", color = Color.green())
                     await interaction.edit_original_response(embed = embed)
                 else:
--- commands/logging.py
+++ commands/logging.py
@@ -3,6 +3,7 @@ from discord import Color, ButtonStyle
 from discord.ext import commands
 from discord.ui import View
 import sqlite3
+import datetime
 
 class logging(commands.Cog):
     def __init__(self, bot):
@@ -13,45 +14,83 @@ class logging(commands.Cog):
     # Message Logger - Delete
     @commands.Cog.listener()
     async def on_message_delete(self, message):
-        # Ignore bots
-        if message.author.bot != True:
-            embed = discord.Embed(title = "Message Deleted", description = message.content, color = Color.red())
-            embed.add_field(name = "User Info", value = f"User: {message.author.mention} ({message.author.name})\nID: {message.author.id}")
-            embed.add_field(name = "Message Info", value = f"Channel: {message.channel.name} - {message.channel.mention}\nMessage ID: {message.id}\nTime Created: <t:{round((message.created_at).timestamp())}:t>")
-            embed.add_field(name = "Message Content", value = (f"{message.content[:1021]}..." if len(message.content) > 1024 else message.content))
-            if message.attachments != []:
-                for attachment in message.attachments:
-                    attachStr = ""
-                    if attachStr == "":
-                        attachStr == f"[{attachment.filename}]({attachment.url})"
-                    else:
-                        attachStr += f", [{attachment.filename}]({attachment.url})"
-                    embed.add_field(name = f"Attachments ({len(message.attachments)} attachments)", value = attachStr)
-        else:
-            pass
+        # Look through logging toggle list
+        for val in self.bot.loggingToggle:
+            # If we find our server in the list...
+            if val[0] == message.guild.id:
+                # And message delete logging is enabled...
+                if val[2] == "True":
+                    # Ignore bots
+                    if message.author.bot != True:
+                        # Create embed
+                        embed = discord.Embed(title = "Message Deleted", description = message.content, color = Color.red())
+                        embed.add_field(name = "User Info", value = f"**User:** {message.author.mention} *(@{message.author.name})*\n**ID:** {message.author.id}", inline = False)
+                        embed.add_field(name = "Message Info", value = f"**Channel:** {message.channel.name} - {message.channel.mention}\n**Message ID:** {message.id}\n**Time Created:** <t:{round((message.created_at).timestamp())}:t>", inline = False)
+                        embed.timestamp = datetime.datetime.now()
+                        
+                        # Add attachments to embed
+                        if message.attachments != []:
+                            for attachment in message.attachments:
+                                attachStr = ""
+                                if attachStr == "":
+                                    attachStr == f"[{attachment.filename}]({attachment.url})"
+                                else:
+                                    attachStr += f", [{attachment.filename}]({attachment.url})"
+                                embed.add_field(name = f"Attachments ({len(message.attachments)} attachments)", value = attachStr)
+                        
+                        # Find logging channel
+                        for val in self.bot.loggingChannel:
+                            if val[0] == message.guild.id:
+                                channel_id = val[2]
+                        
+                        channel = self.bot.get_channel(int(channel_id))
+                        await channel.send(embed = embed)
+                else:
+                    pass
+            else:
+                pass
     
     # Message Logger - Edit
     @commands.Cog.listener()
     async def on_message_edit(self, message_before, message_after):
-        # Ignore bots
-        if message_before.author.bot != True:
-            if message_before.content != message_after.content:
-                embed = discord.Embed(title = "Message Edited", color = Color.yellow())
-                embed.add_field(name = "User Info", value = f"User: {message_before.author.mention} ({message_before.author.name})\nID: {message_before.author.id}", inline = False)
-                embed.add_field(name = "Message Info", value = f"Channel: {message_before.channel.name} - {message_before.channel.mention}\nMessage ID: {message_before.id}\nTime Created: <t:{round((message_before.created_at).timestamp())}:t>", inline = False)
-                embed.add_field(name = "Before Edit", value = (f"{message_before.content[:1021]}..." if len(message_before.content) > 1024 else message_before.content), inline = True)
-                embed.add_field(name = "After Edit", value = (f"{message_after.content[:1021]}..." if len(message_after.content) > 1024 else message_after.content), inline = True)
+        # Look through logging toggle list
+        for val in self.bot.loggingToggle:
+            # If we find our server in the list...
+            if val[0] == message_before.guild.id:
+                # And message edit logging is enabled...
+                if val[3] == "True":
+                    # Ignore bots
+                    if message_before.author.bot != True:
+                        if message_before.content != message_after.content:
+                            embed = discord.Embed(title = "Message Edited", color = Color.yellow())
+                            embed.add_field(name = "User Info", value = f"**User:** {message_before.author.mention} *({message_before.author.name})*\n**ID:** {message_before.author.id}", inline = False)
+                            embed.add_field(name = "Message Info", value = f"**Channel:** {message_before.channel.name} - {message_before.channel.mention}\n**Message ID:** {message_before.id}\n**Time Created:** <t:{round((message_before.created_at).timestamp())}:t>", inline = False)
+                            embed.add_field(name = "Before Edit", value = (f"{message_before.content[:1021]}..." if len(message_before.content) > 1024 else message_before.content), inline = True)
+                            embed.add_field(name = "After Edit", value = (f"{message_after.content[:1021]}..." if len(message_after.content) > 1024 else message_after.content), inline = True)
+                            embed.timestamp = datetime.datetime.now()
 
-                view = View()
-                jumpButton = discord.ui.Button(style = discord.ButtonStyle.url, url = message_after.jump_url, label = "Jump to Message")
-                view.add_item(jumpButton)
+                            view = View()
+                            jumpButton = discord.ui.Button(style = discord.ButtonStyle.url, url = message_after.jump_url, label = "Jump to Message")
+                            view.add_item(jumpButton)
 
-                # channel = self.bot.get_channel(int(channel_id))
-                # await channel.send(message)
+                            # Find logging channel
+                            for val in self.bot.loggingChannel:
+                                if val[0] == message_before.guild.id:
+                                    channel_id = val[3]
+                            
+                            channel = self.bot.get_channel(int(channel_id))
+                            await channel.send(embed = embed)
+
+                            # channel = self.bot.get_channel(int(channel_id))
+                            # await channel.send(message)
+                        else:
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
             else:
                 pass
-        else:
-            pass
             
 async def setup(bot):
     await bot.add_cog(logging(bot))
